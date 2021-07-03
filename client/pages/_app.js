afc9721f2283f9714081d295844650c1d8144ee3
Antonio <antonioenriquez@wolfsellers.com>
2021-07-03 01:40:44 -0500

    Global State Management with useReducer and useContext Hooks

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,13 @@
 // pages/_app.js
 import 'bootstrap/dist/css/bootstrap.css';
 import Nav from '../components/Nav';
+import {Provider} from '../context';
 
 export default function MyApp({ Component, pageProps }) {
-    return<>
+    return(
+    <Provider>
         <Nav />
         <Component {...pageProps} />
-    </>
+    </Provider>
+    )
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,12 @@
+import {Context} from '../context'
+import { useContext } from 'react';
+
 const Home = () => {
+    const {state} = useContext(Context);
     return(
         <div>
             <h2>Home page</h2>
+            {JSON.stringify(state)}
         </div>
     )
 }
