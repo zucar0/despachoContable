e6d9df393c0724f7bf2b9505393e0180be6403d2
Antonio <antonioenriquez@wolfsellers.com>
2021-07-06 02:02:59 -0500

    Firebase Auth State

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,13 +4,16 @@ import Nav from '../components/Nav';
 import {Provider} from '../context';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import FirebaseAuthState from '../components/FirebaseAuthState';
 
 export default function MyApp({ Component, pageProps }) {
     return(
     <Provider>
+        <FirebaseAuthState>
         <Nav />
         <ToastContainer/>
         <Component {...pageProps} />
+        </FirebaseAuthState>
     </Provider>
     )
 }
